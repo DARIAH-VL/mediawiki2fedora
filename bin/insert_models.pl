3e9ae4c79a10e2e186082c9b03fd8f4e8a56c485
njfranck <njfranck@ca20c521.ugent.be>
2015-12-04 17:16:59 +0100

    split into separate objects

---
+++ bin/insert_models.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use lib qw(/home/njfranck/git/Catmandu-FedoraCommons/lib);
+use lib qw(/home/njfranck/git/Catmandu-MediaWiki/lib);
+use Catmandu::Sane;
+use Catmandu -load => ["."];
+use Catmandu::Util qw(:is);
+use MediaWikiFedora qw(:all);
+use File::Basename;
+use File::Spec;
+
+
+my $data_dir = File::Spec->rel2abs( File::Spec->catdir( dirname(__FILE__), "..", "data" ) );
+my @models = qw(pageCModel revisionCModel fileCModel);
+
+my $namespace = Catmandu->config->{namespace} // "mediawiki";
+my $fedora = fedora();
+
+for my $model( @models ){
+
+    my $pid = "${namespace}:$model";
+    my $xml_file = File::Spec->catfile($data_dir,"${model}.xml");
+    my $object_profile;
+    {
+        my $res = $fedora->getObjectProfile(pid => $pid);
+        if( $res->is_ok ) {
+            $object_profile = $res->parse_content();
+        }
+    }
+    unless($object_profile){
+        say "object $pid: ingest";
+        my $res = $fedora->ingest( pid => $pid , file => $xml_file , format => 'info:fedora/fedora-system:FOXML-1.1' );
+        die($res->raw()) unless $res->is_ok();
+    }
+}
--- bin/mediawiki2fedora.pl
+++ bin/mediawiki2fedora.pl
@@ -13,303 +13,274 @@ use RDF::Trine::Node::Literal;
 use RDF::Trine::Serializer;
 use RDF::Trine::Graph;
 
+sub rdf_resource {
+    RDF::Trine::Node::Resource->new($_[0]);
+}
+sub rdf_literal {
+    RDF::Trine::Node::Literal->new($_[0]);
+}
+sub rdf_statement {
+    RDF::Trine::Statement->new(@_);
+}
+sub rdf_graph {
+    RDF::Trine::Graph->new(@_);
+}
+
 my $force = 0;
 
 GetOptions(
     "force" => \$force
 );
 
-my $namespace = Catmandu->config->{namespace} // "mediawiki";
+my $namespace_page = Catmandu->config->{namespace_page} // "mediawiki";
+my $namespace_revision = Catmandu->config->{namespace_revision} // "mediawikirevision";
+my $namespace_file = Catmandu->config->{namespace_file} // "mediawikifile";
 my $ownerId = Catmandu->config->{ownerId} // "mediawiki";
 my $fedora = fedora();
-my $rdf_serializer = RDF::Trine::Serializer->new('rdfxml',namespaces => {
-    dc => "http://purl.org/dc/elements/1.1/"
-});
+
+my $namespaces = {
+    dc => "http://purl.org/dc/elements/1.1/",
+    "fedora-model" => "info:fedora/fedora-system:def/model#",
+    foxml => "info:fedora/fedora-system:def/foxml#",
+    xsi => "http://www.w3.org/2001/XMLSchema-instance",
+    rel => "info:fedora/fedora-system:def/relations-external#"
+};
+
+my $rdf_serializer = RDF::Trine::Serializer->new('rdfxml',namespaces => $namespaces );
 
 Catmandu->importer('mediawiki')->each(sub{
     my $r = shift;
-    my $pid = "${namespace}:".$r->{pageid};
-    my $object_profile;
+
+    #1. add/update page
     {
-        my $res = $fedora->getObjectProfile(pid => $pid);
-        if( $res->is_ok ) {
-            $object_profile = $res->parse_content();
-        }
-    }
-    #1. new object with empty datastream DC
-    if(!defined($object_profile)){
-        say "object $pid: ingest";
-        my $foxml = generate_foxml({ label => $r->{title}, ownerId => ${ownerId} });
-
-        my $res = $fedora->ingest( pid => $pid , xml => $foxml , format => 'info:fedora/fedora-system:FOXML-1.1' );
-        die($res->raw()) unless $res->is_ok();
-    }
-    #2. update datastream DC
-    {
-        my $res = $fedora->getDatastream(pid => $pid, dsID => "DC");
-        if( !$res->is_ok() || $force ) {
-            say "object $pid: modify datastream DC";
-            my $ds_dc = { _id => $pid, title => [$r->{title}], identifier => [$r->{pageid}] };
-            dc->update($ds_dc);
-        }
-    }
-    #3. add revisions as separate datastreams
-    for my $revision(@{ $r->{revisions} }) {
-
-        #add source from mediawiki (reference)
+        #1.1 get page
+        my $pid = "${namespace_page}:".$r->{pageid};
+        my $page_object_profile;
         {
-            my $dsID = "REV.".$revision->{revid}.".SRC";
-            my $datastream;
-            {
-                my $res = $fedora->getDatastream(pid => $pid, dsID => $dsID);
-                if ( $res->is_ok() ) {
-                    $datastream = $res->parse_content();
-                }
-
+            my $res = $fedora->getObjectProfile(pid => $pid);
+            if( $res->is_ok ) {
+                $page_object_profile = $res->parse_content();
             }
-
-            my($fh,$file);
-            my %args;
-
-            if ( !$datastream || $force ) {
-
-                #write content to tempfile
-                ($fh,$file) = tempfile(UNLINK => 1,EXLOCK => 0);
-                binmode $fh,":raw";
-                print $fh json->encode($revision);
-                close $fh;
-
-                %args = (
-                    pid => $pid,
-                    dsID => $dsID,
-                    file => $file,
-                    versionable => "false",
-                    dsLabel => "source for REV.".$revision->{revid},
-                    mimeType => "application/json; charset=utf-8"
-                );
-            }
-
-            if( $datastream ) {
-                if ( $force ) {
-                    say "object $pid: modify datastream $dsID";
-                    my $res = $fedora->modifyDatastream(%args);
-                    die($res->raw()) unless $res->is_ok();
-                }
-            }
-            else{
-                say "adding datastream $dsID to object $pid";
-
-                my $res = $fedora->addDatastream(%args);
-                die($res->raw()) unless $res->is_ok();
-
-            }
-
-            unlink $file if is_string($file) && -f $file;
-
         }
+        #1.2 new page with empty datastream DC
+        if(!defined($page_object_profile)){
+            say "object $pid: ingest";
+            my $foxml = generate_foxml({ label => $r->{title}, ownerId => ${ownerId} });
 
-        #add mediawiki text separately
+            my $res = $fedora->ingest( pid => $pid , xml => $foxml , format => 'info:fedora/fedora-system:FOXML-1.1' );
+            die($res->raw()) unless $res->is_ok();
+        }
+        #1.3 update datastream DC
         {
-
-            my $dsID = "REV.".$revision->{revid};
-            my $datastream;
-            {
-                my $res = $fedora->getDatastream(pid => $pid, dsID => $dsID);
-                if ( $res->is_ok() ) {
-                    $datastream = $res->parse_content();
-                }
+            my $res = $fedora->getDatastream(pid => $pid, dsID => "DC");
+            if( !$res->is_ok() || $force ) {
+                say "object $pid: modify datastream DC";
+                my $ds_dc = { _id => $pid, title => [$r->{title}], identifier => [$r->{pageid}] };
+                dc->update($ds_dc);
             }
-
-            my($fh,$file);
-            my %args;
-            if ( !$datastream || $force ) {
-                #write content to tempfile
-                ($fh,$file) = tempfile(UNLINK => 1,EXLOCK => 0);
-                binmode $fh,":utf8";
-                print $fh $revision->{'*'};
-                close $fh;
-
-                #dsLabel has a maximum of 255 characters
-                my $dsLabel = substr($revision->{parsedcomment} // "",0,255);
-                utf8::encode($dsLabel);
-
-                %args = (
-                    pid => $pid,
-                    dsID => $dsID,
-                    file => $file,
-                    versionable => "false",
-                    dsLabel => $dsLabel,
-                    mimeType => $revision->{contentformat}."; charset=utf-8",
-                    checksumType => "SHA-1",
-                    checksum => $revision->{sha1}
-                );
-            }
-
-            if( $datastream ) {
-                if ( $force ) {
-                    say "object $pid: modify datastream $dsID";
-                    my $res = $fedora->modifyDatastream(%args);
-                    die($res->raw()) unless $res->is_ok();
-                }
-            }
-            else{
-                say "adding datastream $dsID to object $pid";
-
-                my $res = $fedora->addDatastream(%args);
-                die($res->raw()) unless $res->is_ok();
-
-            }
-
-            unlink $file if is_string($file) && -f $file;
         }
-        #mediawiki text converted to HTML
+        #1.4 update RELS-EXT
         {
-            my $dsID = "REV.".$revision->{revid}.".HTML";
-            my $datastream;
-            {
-                my $res = $fedora->getDatastream(pid => $pid, dsID => $dsID);
-                if ( $res->is_ok() ) {
-                    $datastream = $res->parse_content();
-                }
-            }
+            my $rdf_xml;
+            my $old_rdf = RDF::Trine::Model->temporary_model;
+            my $new_rdf = RDF::Trine::Model->temporary_model;
+            my $res = $fedora->getDatastreamDissemination( pid => $pid, dsID => "RELS-EXT" );
+            my $is_new = 1;
+            if( $res->is_ok ){
 
-            my($fh,$file);
-            my %args;
-            if ( !$datastream || $force ) {
+                say "object $pid: RELS-EXT found";
+                $is_new = 0;
 
-                my $content = $revision->{'*'};
+                $rdf_xml = $res->raw();
+                my $parser = RDF::Trine::Parser->new('rdfxml');
+                $parser->parse_into_model(undef,$rdf_xml,$old_rdf);
 
-                #convert internal links => for now: link to search page
-                my $prefix = $fedora->{baseurl}."/objects?pid=true&label=true&title=true&terms=&query=label%3D";
+            }else{
 
-                my $html = wiki2html( $content, { prefix => $prefix } );
-                #write content to tempfile
-                ($fh,$file) = tempfile(UNLINK => 1,EXLOCK => 0);
-                binmode $fh,":utf8";
-                print $fh $html;
-                close $fh;
-
-                #dsLabel has a maximum of 255 characters
-                my $dsLabel = "HTML version of datastream REV.".$revision->{revid};
-                utf8::encode($dsLabel);
-
-                %args = (
-                    pid => $pid,
-                    dsID => $dsID,
-                    file => $file,
-                    versionable => "false",
-                    dsLabel => $dsLabel,
-                    mimeType => "text/html; charset=utf-8",
-                    #TODO
-                    #checksumType => "SHA-1",
-                    #checksum => $revision->{sha1}
-                );
-            }
-
-            if( $datastream ) {
-                if ( $force ) {
-                    say "object $pid: modify datastream $dsID";
-                    my $res = $fedora->modifyDatastream(%args);
-                    die($res->raw()) unless $res->is_ok();
-                }
-            }
-            else{
-                say "adding datastream $dsID to object $pid";
-
-                my $res = $fedora->addDatastream(%args);
-                die($res->raw()) unless $res->is_ok();
+                say "object $pid: RELS-EXT not found";
 
             }
-
-            unlink $file if is_string($file) && -f $file;
-
-        }
-    }
-    #4. update RELS-INT
-    {
-        my $dc_ns = "http://purl.org/dc/elements/1.1/";
-        my $rdf_xml;
-        my $old_rdf = RDF::Trine::Model->temporary_model;
-        my $new_rdf = RDF::Trine::Model->temporary_model;
-        my $res = $fedora->getDatastreamDissemination( pid => $pid, dsID => "RELS-INT" );
-        my $is_new = 1;
-        if( $res->is_ok ){
-
-            say "object $pid: RELS-INT found";
-            $is_new = 0;
-
-            $rdf_xml = $res->raw();
-            my $parser = RDF::Trine::Parser->new('rdfxml');
-            $parser->parse_into_model(undef,$rdf_xml,$old_rdf);
-
-        }else{
-
-            say "object $pid: RELS-INT not found";
-
-        }
-
-
-        for my $revision(@{ $r->{revisions} }){
-            my $subject = "info:fedora/${pid}/REV.".$revision->{revid};
-
-            my @relations;
-            push @relations,{ relation => [ $subject, "${dc_ns}creator", $revision->{user} ], isLiteral => "true" };
-
-            push @relations,{ relation => [ $subject, "${dc_ns}date", $revision->{timestamp} ], isLiteral => "true" };
-            if( is_natural($revision->{parentid}) && $revision->{parentid} > 0 ) {
-
-                push @relations,{ relation => [ $subject, "${dc_ns}relation.isBasedOn", "info:fedora/${pid}/REV.".$revision->{parentid} ], isLiteral => "false" };
-
-            }
-
-            for my $relation(@relations){
-
-                my $rel = $relation->{relation};
-                my $subject_n =  RDF::Trine::Node::Resource->new($rel->[0]);
-                my $predicate_n = RDF::Trine::Node::Resource->new($rel->[1]);
-                my $object_n = RDF::Trine::Node::Literal->new($rel->[2]);
-
-                $new_rdf->add_statement( RDF::Trine::Statement->new( $subject_n, $predicate_n, $object_n ) );
-
-            }
-
-        }
-        my $old_graph = RDF::Trine::Graph->new( $old_rdf );
-        my $new_graph = RDF::Trine::Graph->new( $new_rdf );
-
-        unless( $old_graph->equals($new_graph) ){
-
-            say "object $pid: RELS-INT has changed";
-
-            my $rdf_data = $rdf_serializer->serialize_model_to_string( $new_rdf );
-
-            #write content to tempfile
-            my($fh,$file) = tempfile(UNLINK => 1,EXLOCK => 0);
-            binmode $fh,":utf8";
-            print $fh $rdf_data;
-            close $fh;
-
-            my %args = (
-                pid => $pid,
-                dsID => "RELS-INT",
-                file => $file,
-                versionable => "true",
-                dsLabel => "Fedora Relationship Metadata.",
-                mimeType => "application/rdf+xml"
+            $new_rdf->add_statement(
+                rdf_statement(
+                    rdf_resource("info:fedora/${pid}"),
+                    rdf_resource($namespaces->{rel}."isCollection"),
+                    rdf_literal("true")
+                )
+            );
+            $new_rdf->add_statement(
+                rdf_statement(
+                    rdf_resource("info:fedora/${pid}"),
+                    rdf_resource($namespaces->{'fedora-model'}."hasModel"),
+                    rdf_resource("info:fedora/mediawiki:pageCModel")
+                )
             );
 
-            my $r;
-            if($is_new){
-                $r = $fedora->addDatastream(%args);
-            }else{
-                $r = $fedora->modifyDatastream(%args);
-            }
-            die($r->raw()) unless $r->is_ok();
-            if($is_new){
-                say "object $pid: datastream RELS-INT added";
-            }else{
-                say "object $pid: datastream RELS-INT updated";
+            my $old_graph = rdf_graph( $old_rdf );
+            my $new_graph = rdf_graph( $new_rdf );
+
+            unless( $old_graph->equals($new_graph) ){
+
+                say "object $pid: RELS-EXT has changed";
+
+                my $rdf_data = $rdf_serializer->serialize_model_to_string( $new_rdf );
+
+                #write content to tempfile
+                my($fh,$file) = tempfile(UNLINK => 1,EXLOCK => 0);
+                binmode $fh,":utf8";
+                print $fh $rdf_data;
+                close $fh;
+
+                my %args = (
+                    pid => $pid,
+                    dsID => "RELS-EXT",
+                    file => $file,
+                    versionable => "true",
+                    dsLabel => "Fedora Object to Object Relationship Metadata.",
+                    mimeType => "application/rdf+xml"
+                );
+
+                my $r;
+                if($is_new){
+
+                    $r = $fedora->addDatastream(%args);
+
+                }else{
+
+                    $r = $fedora->modifyDatastream(%args);
+
+                }
+
+                die($r->raw()) unless $r->is_ok();
+
+                if($is_new){
+
+                    say "object $pid: datastream RELS-EXT added";
+
+                }else{
+
+                    say "object $pid: datastream RELS-EXT updated";
+
+                }
             }
         }
+
+    }
+
+    #2. add revisions as separate objects
+    for my $revision(@{ $r->{revisions} }) {
+
+        my $pid = "${namespace_revision}:".$revision->{revid};
+
+        #2.1 get revision
+        my $rev_object_profile;
+        {
+            my $res = $fedora->getObjectProfile(pid => $pid);
+            if( $res->is_ok ) {
+                $rev_object_profile = $res->parse_content();
+            }
+        }
+        #2.2 new revision with empty datastream DC
+        if(!defined($rev_object_profile)){
+            say "object $pid: ingest";
+            my $foxml = generate_foxml({ label => $r->{title}, ownerId => ${ownerId} });
+
+            my $res = $fedora->ingest( pid => $pid , xml => $foxml , format => 'info:fedora/fedora-system:FOXML-1.1' );
+            die($res->raw()) unless $res->is_ok();
+        }
+        #2.3 update datastream DC
+        {
+            my $res = $fedora->getDatastream(pid => $pid, dsID => "DC");
+            if( !$res->is_ok() || $force ) {
+                say "object $pid: modify datastream DC";
+                my $ds_dc = { _id => $pid, title => [$r->{title}], identifier => [$r->{pageid}] };
+                dc->update($ds_dc);
+            }
+        }
+        #2.4 update RELS-EXT
+        {
+            my $rdf_xml;
+            my $old_rdf = RDF::Trine::Model->temporary_model;
+            my $new_rdf = RDF::Trine::Model->temporary_model;
+            my $res = $fedora->getDatastreamDissemination( pid => $pid, dsID => "RELS-EXT" );
+            my $is_new = 1;
+            if( $res->is_ok ){
+
+                say "object $pid: RELS-EXT found";
+                $is_new = 0;
+
+                $rdf_xml = $res->raw();
+                my $parser = RDF::Trine::Parser->new('rdfxml');
+                $parser->parse_into_model(undef,$rdf_xml,$old_rdf);
+
+            }else{
+
+                say "object $pid: RELS-EXT not found";
+
+            }
+            $new_rdf->add_statement(
+                rdf_statement(
+                    rdf_resource("info:fedora/${pid}"),
+                    rdf_resource($namespaces->{rel}."isMemberOf"),
+                    rdf_resource("info:fedora/${namespace_page}:".$r->{pageid})
+                )
+            );
+            $new_rdf->add_statement(
+                rdf_statement(
+                    rdf_resource("info:fedora/${pid}"),
+                    rdf_resource($namespaces->{'fedora-model'}."hasModel"),
+                    rdf_resource("info:fedora/mediawiki:revisionCModel")
+                )
+            );
+
+            my $old_graph = rdf_graph( $old_rdf );
+            my $new_graph = rdf_graph( $new_rdf );
+
+            unless( $old_graph->equals($new_graph) ){
+
+                say "object $pid: RELS-EXT has changed";
+
+                my $rdf_data = $rdf_serializer->serialize_model_to_string( $new_rdf );
+
+                #write content to tempfile
+                my($fh,$file) = tempfile(UNLINK => 1,EXLOCK => 0);
+                binmode $fh,":utf8";
+                print $fh $rdf_data;
+                close $fh;
+
+                my %args = (
+                    pid => $pid,
+                    dsID => "RELS-EXT",
+                    file => $file,
+                    versionable => "true",
+                    dsLabel => "Fedora Object to Object Relationship Metadata.",
+                    mimeType => "application/rdf+xml"
+                );
+
+                my $r;
+                if($is_new){
+
+                    $r = $fedora->addDatastream(%args);
+
+                }else{
+
+                    $r = $fedora->modifyDatastream(%args);
+
+                }
+
+                die($r->raw()) unless $r->is_ok();
+
+                if($is_new){
+
+                    say "object $pid: datastream RELS-EXT added";
+
+                }else{
+
+                    say "object $pid: datastream RELS-EXT updated";
+
+                }
+            }
+
+        }
+
     }
 });
